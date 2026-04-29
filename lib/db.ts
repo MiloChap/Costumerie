04e248f01938d00deb4b60a53d887ef2eac3c713
milo chapat <mchapat1998@gmail.com>
2026-04-29 08:44:04 +0200

    fix prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,19 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client/edge"
 
 const createPrismaClient = () => {
   const adapter = new PrismaNeon({
     connectionString: process.env.DATABASE_URL!,
-  });
-  return new PrismaClient({ adapter });
-};
+  })
+  return new PrismaClient({ adapter })
+}
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
+  prisma: ReturnType<typeof createPrismaClient> | undefined
+}
 
-export const prisma =
-  globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }
